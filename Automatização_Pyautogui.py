bbe55eaa662cd1f1f79acb69704f65a9fdda9c58
JESUS <marcos.jesus.py25@gmail.com>
2025-06-23 17:47:56 -0300

    Add files via upload

---
+++ Automatização_Pyautogui.py
@@ -0,0 +1,15 @@
+pyautogui.press("Win") # Usando para  prescionar uma tecla desejada
+pyautogui.sleep(1) # Usado para  ajustar o tempo de execução entre uma   ordem e outra 
+pyautogui.write("Google chrome", interval = 0.2) # Utilizado para escrever 
+pyautogui.press("Enter")
+pyautogui.sleep(1)
+pyautogui.write("Senai - santana de Parnaiba ", interval = 0.1)
+pyautogui.press("Enter")
+pyautogui.sleep(1)
+Pyautogui.moveTo(906,507, duration = 0.4)
+pyautogui.click()
+pyautogui.sleep(1)
+pyautogui.hotkey("shift","win","s")
+
+
+
